41f77c6b394f136b091c77980d2b719ca2092dc7
Rafe Lepre <rafe.lepre@skedge.me>
2016-09-21 14:46:57 -0400

    updates

--- source/data/ui.js
+++ source/data/ui.js
@@ -17,6 +17,26 @@ let
     subscribe: func => uiStore.subscribe( func ),
     getState: () => uiStore.getState(),
     setState: data => uiStore.dispatch( { type: 'set', data } )
-  };
+  },
+  lastView = uiStore.getState().view;
+
+window.setTimeout( function() {
+  let application = document.querySelector( 'section' );
+
+  ui.subscribe( () => {
+
+    const { view } = ui.getState();
+
+    if ( lastView !== view ) {
+      lastView = view;
+      if ( lastView !== 'Landing' ) {
+        Array.from( application.querySelectorAll( 'section' ) ).forEach( el => {
+          el.scrollTop = 0;
+        } );
+      }
+    }
+
+  } )
+}, 100 );
 
 export default ui
